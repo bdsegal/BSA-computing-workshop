6fa5b287fa640f6f7c3be88ea65fb3387990252a
bdsegal <bdsegal@umich.edu>
2016-02-11 23:24:16 -0500

    revised content

--- data/download_files.R
+++ data/download_files.R
@@ -8,23 +8,23 @@ parent <- dirname(getwd())
 # EIA residential energy consumption survey (RECS) 27 MB csv file
 download.file(url="http://www.eia.gov/consumption/residential/data/2009/csv/recs2009_public.csv",
   mode="wb",
-  destfile=file.path(parent,'data','recs2009.csv'))
+  destfile='recs2009.csv')
 
 # data description for RECS, 59 KB file
 download.file(url="http://www.eia.gov/consumption/residential/data/2009/csv/public_layout.csv",
   mode="wb",
-  destfile=file.path(parent,'data','public_layout.csv'))
+  destfile='public_layout.csv')
   
 # Porto taxi cab data, 520 MB zip file
 download.file(url="https://archive.ics.uci.edu/ml/machine-learning-databases/00339/train.csv.zip",
-  destfile=file.path(parent,'data','train.zip'))
+  destfile='train.zip')
 
 unzip("train.zip")
 
 # # weather data, 206 MB gz file
 # download.file(url="http://www1.ncdc.noaa.gov/pub/data/ghcn/daily/by_year/2010.csv.gz",
   # mode="wb",
-  # destfile=file.path(parent, 'data', '2010.csv.gz'))
+  # destfile='2010.csv.gz')
 
 # # unzip, but don't remove the original gz file
 # gunzip("2010.csv.gz", remove=FALSE)

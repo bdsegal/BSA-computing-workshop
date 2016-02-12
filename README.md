04b094ec82c9df1ed7bf0cf3cc0620ca69681f31
bdsegal <bdsegal@umich.edu>
2016-02-12 16:33:21 -0500

    updated module numbers

--- README.md
+++ README.md
@@ -3,17 +3,17 @@
 
 These notes go over some aspects of:
 
-0. Downloading data in R
-1. Managing large desktop-sized files (a few GB in size)
-2. Assessing parsing errors from `readr`
-3. Manipulating data quickly with `dplyr` and `data.table` (in progress)
-4. Implementing the split, apply, combine approach with `dplyr`, and reshaping and plotting with `reshape2` and `ggplot2`
-5. Plotting with `ggvis`
+1. Downloading data in R
+2. Managing large desktop-sized files (a few GB in size)
+3. Assessing parsing errors from `readr`
+4. Manipulating data quickly with `dplyr` and `data.table` (in progress)
+5. Implementing the split, apply, combine approach with `dplyr`, and reshaping and plotting with `reshape2` and `ggplot2`
+6. Plotting with `ggvis`
 
 I might also put together notes on visualizing missing data with `pheatmap` and `mi`.
 
-All html files are in the `html` folder, and the R markdown files are in the `markdown` folder. You can also view the html files [here](http://bdsegal.github.io/BSA-computing-workshop/).
+You can view the html files as web pages [here](http://bdsegal.github.io/BSA-computing-workshop/).
 
-To run tutorials 1-4, you'll either need to replicate the directory structure on your computer or change the path to the data folder. One way to replicate the directory structure is to clone this repository.
+To run tutorials 1-4, you'll either need to replicate the directory structure on your computer or change `dataPath` (at the top of each tutorial)  to wherever you downloaded the data. One way to replicate the directory structure is to clone this repository. If you clone the repository, you can run the code directly from the Rmd files.
 
 Thanks, and happy computing!
--- markdown/makeHTML.R
+++ markdown/makeHTML.R
@@ -13,56 +13,56 @@ library(rmarkdown)
 
 parent <- dirname(getwd())
 
-# 0. tips for downloading data in R
-render("0.downloading_data.Rmd",
+# 1. tips for downloading data in R
+render("1.downloading_data.Rmd",
   output_format = html_document(number_sections = TRUE, toc = TRUE),
   output_dir = file.path(parent, "html"),
-  output_file = "0.downloading_data.html")
+  output_file = "1.downloading_data.html")
   
 browseURL(paste("file://",
-  file.path(parent, "html", "0.downloading_data.html"), sep = ""))
+  file.path(parent, "html", "1.downloading_data.html"), sep = ""))
 
-# 1. managing large files
-render("1.managing_large_files.Rmd",
+# 2. managing large files
+render("2.managing_large_files.Rmd",
   output_format = html_document(number_sections = TRUE, toc = TRUE),
   output_dir = file.path(parent, "html"),
-  output_file = "1.managing_large_files.html")
+  output_file = "2.managing_large_files.html")
 
 browseURL(paste("file://",
-  file.path(parent, "html", "1.managing_large_files.html"), sep = ""))
+  file.path(parent, "html", "2.managing_large_files.html"), sep = ""))
 
-# 2.assessing_parsing_errors
-render("2.assessing_parsing_errors.Rmd",
+# 3.assessing_parsing_errors
+render("3.assessing_parsing_errors.Rmd",
   output_format = html_document(number_sections = TRUE, toc = TRUE),
   output_dir = file.path(parent, "html"),
-  output_file = "2.assessing_parsing_errors.html")
+  output_file = "3.assessing_parsing_errors.html")
 
 browseURL(paste("file://",
-  file.path(parent, "html", "2.assessing_parsing_errors.html"), sep = ""))
+  file.path(parent, "html", "3.assessing_parsing_errors.html"), sep = ""))
 
-# 3.dplyr_data.table
-render("3.dplyr_data.table.Rmd",
+# 4.dplyr_data.table
+render("4.dplyr_data.table.Rmd",
   output_format = html_document(number_sections = TRUE, toc = TRUE),
   output_dir = file.path(parent, "html"),
-  output_file = "3.dplyr_data.table.html")
+  output_file = "4.dplyr_data.table.html")
 
 browseURL(paste("file://",
-  file.path(parent, "html", "3.dplyr_data.table.html"), sep = ""))
+  file.path(parent, "html", "4.dplyr_data.table.html"), sep = ""))
 
-# 4. split_apply_combine_plot
-render("4.split_apply_combine_plot.Rmd",
+# 5. split_apply_combine_plot
+render("5.split_apply_combine_plot.Rmd",
   output_format = html_document(number_sections = TRUE, toc = TRUE),
   output_dir = file.path(parent, "html"),
-  output_file = "4.split_apply_combine_plot.html")
+  output_file = "5.split_apply_combine_plot.html")
 
 browseURL(paste("file://",
-  file.path(parent, "html", "4.split_apply_combine_plot.html"), sep = ""))
+  file.path(parent, "html", "5.split_apply_combine_plot.html"), sep = ""))
 
 # 5.ggvis
-render("5.ggvis.Rmd",
+render("6.ggvis.Rmd",
   output_format = html_document(number_sections = TRUE, toc = TRUE),
   output_dir = file.path(parent, "html"),
-  output_file = "5.ggvis.html")
+  output_file = "6.ggvis.html")
 
 browseURL(paste("file://",
-  file.path(parent, "html", "5.ggvis.html"), sep = ""))
+  file.path(parent, "html", "6.ggvis.html"), sep = ""))
